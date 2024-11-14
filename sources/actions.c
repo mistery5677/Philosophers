d0570b45bd20f4993f27db12f1f6b26e735046cb
mistery5677 <miguel.costa.5677@gmail.com>
2024-11-14 00:49:56 +0000

    Ja arranjei maneira para verificar quando o programa deve de parar ... mas ainda nao esta a parar

--- sources/actions.c
+++ sources/actions.c
@@ -7,13 +7,14 @@ void	eat(t_philo *philo)
 	printf("%d %d has taken a fork\n", get_current_time() - philo->data->start_time, philo->name);
 	printf("%d %d has taken a fork\n", get_current_time() - philo->data->start_time, philo->name);
 	printf("%d %d is eating\n", get_current_time() - philo->data->start_time, philo->name);
-	philo->last_meal = get_current_time() - philo->data->start_time;
 	philo->eat_times++;
+	philo->last_meal = 0;
 	if (ft_usleep(philo, philo->data->time_eat) == -1 && philo->data->died == 0)
 	{
 		philo->data->died = philo->name;
 		printf("%d %d died\n", get_current_time() - philo->data->start_time, philo->name);
 	}
+	philo->last_meal = get_current_time();
 	//printf("ocurrent time %d\n", get_current_time(philo->data));
 	pthread_mutex_unlock(philo->l_fork);
 	pthread_mutex_unlock(philo->r_fork);
--- sources/time_zone.c
+++ sources/time_zone.c
@@ -17,10 +17,14 @@ int	ft_usleep(t_philo *philo, size_t milliseconds)
 	size_t	start;
 
 	start = get_current_time();
+    //printf("last meal %d\n", philo->last_meal);
 	while ((get_current_time() - start) < milliseconds)
     {
-        if ((get_current_time() - philo->last_meal) - philo->data->start_time > philo->data->time_die)
+        //printf("philo name %d     current_time() - philo->last_meal %d\n", philo->name, get_current_time() - philo->last_meal);
+        if (philo->last_meal != 0 && (get_current_time() - philo->last_meal) > philo->data->time_die)
         {
+            //printf("Morreu %d\n", get_current_time() - philo->last_meal);
+            //printf("Philo %d (get_current_time() - philo->last_meal) - philo->data->start_time %d philo->data->time_die %d\n", philo->name, (get_current_time() - philo->last_meal) - philo->data->start_time, philo->data->time_die);
             //philo->data->died = philo->name;
             return (-1);
         }
