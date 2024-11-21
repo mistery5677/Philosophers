5d94a79ddacaaa374a299c18a96c8efb43c39c86
mistery5677 <miguel.costa.5677@gmail.com>
2024-11-21 01:00:02 +0000

    Fixed some bugs

--- philo_norm/sources/actions.c
+++ philo_norm/sources/actions.c
@@ -6,7 +6,7 @@
 /*   By: mistery576 <mistery576@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 16:28:14 by mistery576        #+#    #+#             */
-/*   Updated: 2024/11/21 00:51:17 by mistery576       ###   ########.fr       */
+/*   Updated: 2024/11/21 00:56:53 by mistery576       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,10 @@ void	eat(t_philo *philo)
 	printf("%d %d has taken a fork\n", current_time_ml(philo), philo->name);
 	printf("%d %d has taken a fork\n", current_time_ml(philo), philo->name);
 	printf("%d %d is eating\n", current_time_ml(philo), philo->name);
-	pthread_mutex_unlock(&philo->data->write);
 	if (philo->last_meal == 0)
 		philo->last_meal = get_current_time();
 	philo->eat_times++;
+	pthread_mutex_unlock(&philo->data->write);
 	if (ft_usleep(philo, philo->data->time_eat) == -1 && philo->data->died == 0)
 	{
 		printf("Vai largar os garfos erro%d\n", philo->name);
@@ -46,22 +46,26 @@ void	eat(t_philo *philo)
 
 void	ft_sleep(t_philo *philo)
 {
+	pthread_mutex_lock(&philo->data->write);
 	if (philo->data->philos_finished != philo->data->philos_num && philo->last_meal != 0 && philo->data->died == 0)
 	{
-		pthread_mutex_lock(&philo->data->write);
 		printf("%d %d is sleeping\n", current_time_ml(philo), philo->name);
 		pthread_mutex_unlock(&philo->data->write);
 		ft_usleep(philo, philo->data->time_sleep);
 	}
+	else 
+		pthread_mutex_unlock(&philo->data->write);
 }
 
 void	thinking(t_philo *philo)
 {
-	if (philo->data->time_think != 0 && philo->data->died == 0)
+	pthread_mutex_lock(&philo->data->write);
+	if (philo->data->time_think != 0 && philo->data->died == 0 && philo->data->philos_finished != philo->data->philos_num)
 	{
-		pthread_mutex_lock(&philo->data->write);
 		printf("%d %d is thinking\n", current_time_ml(philo), philo->name);
 		pthread_mutex_unlock(&philo->data->write);
 		ft_usleep(philo, philo->data->time_think);
 	}
+	else
+		pthread_mutex_unlock(&philo->data->write);
 }
